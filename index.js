60a4b383420074a4f38f8463157f41483ffbc01e
Muhammad Ahmed <muhammadahmed7274@gmail.com>
2025-04-30 10:54:01 +0500

    did some changes in js code

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 window.onload = function () {
   const loader = document.getElementById("loader-box");
+  document.body.classList.add("no-scroll");
 
   setTimeout(() => {
     loader.classList.add("hide");
     setTimeout(() => {
       loader.style.display = "none";
+      document.body.classList.remove("no-scroll");
     }, 1000);
   }, 3000);
 };
