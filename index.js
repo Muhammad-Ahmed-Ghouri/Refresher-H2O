b6dd6b48a01346e29408d1f5cee867cfb49303ff
Muhammad Ahmed <muhammadahmed7274@gmail.com>
2025-05-05 21:29:13 +0500

    added js code in shopping cart page

--- index.js
+++ index.js
@@ -27,3 +27,66 @@ function quantityCount() {
 quantity.addEventListener("input", () => {
   quantityCount();
 });
+
+const submitButton = document.getElementById("submit-btn");
+const arrayOfProductDetails = [
+  {
+    customerName: "",
+    customerAddress: "",
+    customerMobileNo: "",
+  },
+  {
+    productName: "19 Lit",
+    quantity: 1,
+    perUnitPrice: 100,
+  },
+];
+
+const customerName = document.getElementById("name");
+const customerMobileNo = document.getElementById("phone");
+const customerAddress = document.getElementById("address");
+
+function checkFields() {
+  if (
+    customerName.value.length >= 3 &&
+    customerMobileNo.value.length >= 8 &&
+    customerAddress.value.length >= 10 &&
+    quantity.value > 0
+  ) {
+    submitButton.disabled = false;
+    submitButton.style.backgroundColor = "#f16320";
+    submitButton.style.cursor = "pointer";
+  } else {
+    submitButton.disabled = true;
+    submitButton.style.backgroundColor = "rgb(203, 199, 199)";
+  }
+}
+
+function arrayOfInformation() {
+  arrayOfProductDetails[0].customerName = customerName.value;
+  arrayOfProductDetails[0].customerAddress = customerAddress.value;
+  arrayOfProductDetails[0].customerMobileNo = customerMobileNo.value;
+  arrayOfProductDetails[1].quantity = quantity.value;
+  console.log(arrayOfProductDetails);
+}
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  arrayOfInformation();
+});
+
+quantity.addEventListener("input", () => {
+  checkFields();
+});
+
+customerName.addEventListener("input", () => {
+  checkFields();
+});
+
+customerMobileNo.addEventListener("input", () => {
+  checkFields();
+});
+
+customerAddress.addEventListener("input", () => {
+  checkFields();
+});
