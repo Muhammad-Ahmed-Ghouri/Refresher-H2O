6149f2f342f89a2d24e3063fdae312d9e8f8b029
Muhammad Ahmed <muhammadahmed7274@gmail.com>
2025-05-05 12:53:22 +0500

    wrote js code for calculate total amount

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 window.onload = function () {
-  const loader = document.getElementById("loader-box");
-  document.body.classList.add("no-scroll");
+  if (window.location.pathname === "/index.html") {
+    const loader = document.getElementById("loader-box");
+    document.body.classList.add("no-scroll");
 
-  setTimeout(() => {
-    loader.classList.add("hide");
     setTimeout(() => {
-      loader.style.display = "none";
-      document.body.classList.remove("no-scroll");
-    }, 1000);
-  }, 3000);
+      loader.classList.add("hide");
+      setTimeout(() => {
+        loader.style.display = "none";
+        document.body.classList.remove("no-scroll");
+      }, 1000);
+    }, 3000);
+  }
 };
+
+const quantity = document.getElementById("quantity");
+const unitPrice = parseInt(document.getElementById("total-amount").innerHTML);
+function quantityCount() {
+  const quantity = document.getElementById("quantity").value;
+  if (quantity >= 0) {
+    let finalAmount = "";
+    finalAmount = quantity * unitPrice;
+    document.getElementById("total-amount").innerHTML = finalAmount;
+    console.log(finalAmount);
+  }
+}
+
+quantity.addEventListener("input", () => {
+  quantityCount();
+});
