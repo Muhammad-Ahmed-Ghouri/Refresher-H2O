7d8b3495bc8af63532b7f3c0349e51378f6380bc
Muhammad-Ahmed-Ghouri <muhammadahmed7274@gmail.com>
2025-10-04 14:23:55 +0500

    removed loader from website beginning

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-window.onload = function () {
-  const loader = document.getElementById("loader-box");
-  document.body.classList.add("no-scroll");
+// window.onload = function () {
+//   const loader = document.getElementById("loader-box");
+//   document.body.classList.add("no-scroll");
 
-  setTimeout(() => {
-    loader.classList.add("hide");
+//   setTimeout(() => {
+//     loader.classList.add("hide");
 
-    setTimeout(() => {
-      loader.style.display = "none";
-      document.body.classList.remove("no-scroll");
-    }, 1000);
-  }, 1000);
-};
+//     setTimeout(() => {
+//       loader.style.display = "none";
+//       document.body.classList.remove("no-scroll");
+//     }, 1000);
+//   }, 1000);
+// };
